f8ba1fd2b2a0f61e1a7748bbef51363631e5a3d9
Saeed Abdilahi <saeedabdilahi@148-100-171-101.foxnet.marist.edu>
2023-03-22 17:51:33 -0400

    done

--- server/app.js
+++ server/app.js
@@ -2,31 +2,36 @@
 const loginForm = document.getElementById("login-form");
 
 // Add an event listener for when the login form is submitted
-loginForm.addEventListener("submit", function(event) {
+loginForm.addEventListener("submit", function (event) {
   // Get references to the username and password input fields
   const usernameInput = document.getElementById("login-username");
   const passwordInput = document.getElementById("login-password");
-  
+
   // Check that both fields are non-empty; if either is empty, prevent the form from submitting and display an error message
   if (!usernameInput.value || !passwordInput.value) {
     event.preventDefault();
     alert("Please enter both a username and password");
+  } 
+  else {
+    alert("Login successful! Welcome, " + usernameInput.value);
   }
 });
 
 // Get a reference to the sign up form
 const signUpForm = document.getElementById("signup-form");
 
-// Add an event listener for when the sign up form is submitted
-signUpForm.addEventListener("submit", function(event) {
-  // Prevent the form from submitting
+signUpForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // Set the text content of the confirmation message
-  confirmationMessage.textContent = "Thank you for signing up!";
-  
-  // Remove the avatar and form from the sign up container, and add the confirmation message
-  signUpContainer.removeChild(signUpAvatar);
-  signUpContainer.removeChild(signUpForm);
-  signUpContainer.appendChild(confirmationMessage);
+  // Get references to the input fields
+  const usernameInput = document.getElementById("signup-username");
+  const emailInput = document.getElementById("signup-email");
+  const passwordInput = document.getElementById("signup-password");
+
+  // Check that all fields are non-empty; if any are empty, display an error message
+  if (!usernameInput.value || !emailInput.value || !passwordInput.value) {
+    alert("Please enter all required fields");
+  } else {
+    alert("Thank you for signing up, " + usernameInput.value + "! You may now log in.");
+  }
 });
